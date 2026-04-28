d8b3739926b3712edbb79cffe0d119d3ede9d976
hidayatulkarimah487-cp.cls <hidayatulkarimah487@gmail.com>
2026-04-28 20:11:30 +0700

    langkah pertama

--- lib/main.dart
+++ lib/main.dart
@@ -9,11 +9,38 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return const MaterialApp(home: ListUserDataPage());
+  }
+}
+
+class ListUserDataPage extends StatefulWidget {
+  const ListUserDataPage({super.key});
+
+  @override
+  State<ListUserDataPage> createState() => _ListUserDataPageState();
+}
+
+class _ListUserDataPageState extends State<ListUserDataPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("User List")),
+      body: ListView.builder(
+        
+        itemCount: 15,
+        itemBuilder: (cxt, i) =>
+            ListTile(title: Text("Nama"), subtitle: Text("umur: 14 tahun"), trailing: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                TextButton(onPressed: () => {}, child: Icon(Icons.edit)),
+                TextButton(onPressed: () => {}, child: Icon(Icons.delete)),
+                ],
+            )
+          ),
+      )
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => {}, 
+        child: Icon(Icons.add),
       ),
     );
   }
