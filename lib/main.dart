cadd4e373c9ebf0dabd1d6ad08ce1b2f605a31d9
hidayatulkarimah487-cp.cls <hidayatulkarimah487@gmail.com>
2026-04-30 13:11:32 +0700

    tugas p7

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(const MainApp());
@@ -20,26 +21,137 @@ class ListUserDataPage extends StatefulWidget {
   State<ListUserDataPage> createState() => _ListUserDataPageState();
 }
 
+class UserModel {
+  int? id;
+  String nama = "";
+  int umur = 0;
+
+  UserModel(this.id, {required this.nama, required this.umur});
+}
+
 class _ListUserDataPageState extends State<ListUserDataPage> {
+  final TextEditingController _nameCtrl = TextEditingController();
+  final TextEditingController _umurCtrl = TextEditingController();
+
+  List<UserModel> userList = [
+    UserModel(1, nama: "Hidaaa", umur: 19),
+    UserModel(2, nama: "Rashya", umur: 7),
+    UserModel(3, nama: "Frieda", umur: 17),
+    UserModel(4, nama: "Lilia", umur: 16),
+  ];
+
+  void _form(int? id) {
+    if (id != null) {
+      var user = userList.firstWhere((data) => data.id == id);
+      _nameCtrl.text = user.nama;
+      _umurCtrl.text = user.umur.toString();
+    } else {
+      _nameCtrl.clear();
+      _umurCtrl.clear();
+    }
+
+    showModalBottomSheet(
+      context: context,
+      isScrollControlled: true,
+      builder: (context) => Padding(
+        padding: EdgeInsetsGeometry.fromLTRB(
+          20,
+          20,
+          20,
+          MediaQuery.of(context).viewInsets.bottom + 50,
+        ),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            TextField(
+              controller: _nameCtrl,
+              decoration: InputDecoration(hintText: "Nama"),
+            ),
+            TextField(
+              controller: _umurCtrl,
+              decoration: InputDecoration(hintText: "Umur"),
+              keyboardType: TextInputType.number,
+            ),
+            ElevatedButton(
+              onPressed: () =>
+                  _save(id, _nameCtrl.text, int.parse(_umurCtrl.text)),
+              child: Text(id == null ? "Tambah" : "Perbaharui"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _save(int? id, String nama, int umur) {
+    if (id != null) {
+      var user = userList.firstWhere((data) => data.id == id);
+      setState(() {
+        user.nama = nama;
+        user.umur = umur;
+      });
+    } else {
+      var nextId = userList.length + 1;
+      var newUser = UserModel(nextId, nama: nama, umur: umur);
+      setState(() {
+        userList.add(newUser);
+      });
+    }
+
+    Navigator.pop(context);
+  }
+
+  void _delete(int id) {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text("Konfirmasi Hapus"),
+        content: Text("Apakah anda yakin ingin menghapus data ini?"),
+        actions: [
+          TextButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: Text("Batal"),
+          ),
+          TextButton(
+            onPressed: () {
+              setState(() => userList.removeWhere((data) => data.id == id));
+              Navigator.pop(context);
+            },
+            child: Text("Hapus"),
+          ),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("User List")),
       body: ListView.builder(
-        
-        itemCount: 15,
-        itemBuilder: (cxt, i) =>
-            ListTile(title: Text("Nama"), subtitle: Text("umur: 14 tahun"), trailing: Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                TextButton(onPressed: () => {}, child: Icon(Icons.edit)),
-                TextButton(onPressed: () => {}, child: Icon(Icons.delete)),
-                ],
-            )
+        itemCount: userList.length,
+        itemBuilder: (xt, i) => ListTile(
+          title: Text(userList[i].nama),
+          subtitle: Text("umur: ${userList[i].umur} tahun"),
+          trailing: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextButton(
+                onPressed: () => _form(userList[i].id),
+                child: Icon(Icons.edit),
+              ),
+              TextButton(
+                onPressed: () => _delete(userList[i].id!),
+                child: Icon(Icons.delete),
+              ),
+            ],
           ),
-      )
+        ),
+      ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () => {}, 
+        onPressed: () => _form(null),
         child: Icon(Icons.add),
       ),
     );
